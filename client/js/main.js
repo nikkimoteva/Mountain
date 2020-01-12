4824d0bd1fa4b6212785080eeda2b042d1297dee
noxolica <angelokn14@gmail.com>
2020-01-12 12:53:55 -0800

    added new json structure

--- client/js/main.js
+++ client/js/main.js
@@ -1,23 +1,24 @@
 const countryInputForm = document.getElementById("countrySelect");
 
-const normalize = (val, max, min) => { return (val - min) / (max - min); }
 
 function updateMap(e) {
   console.log(e);
 
   const countryInput = e.target.elements[0].value.toUpperCase();
-
   const countryPromise = getSentiments(countryInput);
 
   countryPromise
     .then(countries => {
       const updateMapObj = {}
       countries.forEach(country => {
-        updateMapObj[country["target"]] = country["sentiment"]
+        updateMapObj[country["target"]] = { "sentiment": country["sentiment"] * 100 }
       })
       return updateMapObj
     })
-    .then(updateMapObj => { console.log(updateMapObj) })
+    .then(updateMapObj => {
+      // updateMap(updateMapObj) 
+      console.log(updateMapObj);
+    })
     .catch(err => { console.error(err) }
     )
 
