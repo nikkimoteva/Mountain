34b8202568a95b2112d2386c93d07339706ac1b6
noxolica <angelokn14@gmail.com>
2020-01-12 13:47:12 -0800

    last commit

--- client/js/map_control.js
+++ client/js/map_control.js
@@ -1,16 +1,51 @@
 const map = new Datamap({
-  element: document.getElementById('choroplethMap')
+  element: document.getElementById('choroplethMap'),
+  fills: {
+    defaultFill: "#ABDDA4"
+  },
+  // setProjection: "mercator",
+  geographyConfig: {
+    highlightBorderColor: '#bada55',
+    popupTemplate: function (geography, data) {
+      return '<div class="hoverinfo">' + geography.properties.name + '</p>Sentiment: ' + data.sentiment + '</p></div>'
+    },
+    highlightBorderWidth: 3
+  },
 });
 
+var colors = d3.scale.category10();
 
-// object.onload = function(){
+const updateMapCanvas = (mapData) => {
+  map.
+    map.updateChoropleth(mapData);
+};
 
-// };
+const countryInputForm = document.getElementById("countrySelect");
 
-// var basic_choropleth = new Datamap({
-//     element: document.getElementById("basic_choropleth"),
-//     projection: 'mercator',
-//     fills: {
-//       defaultFill: "#ABDDA4"
-//     }
-//   });
+function updateMap(e) {
+  console.log(e);
+
+  const countryInput = e.target.elements[0].value.toUpperCase();
+  const countryPromise = getSentiments(countryInput);
+
+  countryPromise
+    .then(countries => {
+      const updateMapObj = {}
+      countries.forEach(country => {
+        updateMapObj[country["target"]] = { "sentiment": country["sentiment"] * 100 }
+      })
+      return updateMapObj
+    })
+    .then(updateMapObj => {
+      updateMapCanvas(updateMapObj)
+      console.log(updateMapObj);
+    })
+    .catch(err => { console.error(err) }
+    )
+
+  e.preventDefault();
+}
+
+countryInputForm.onsubmit = updateMap;
+
+
