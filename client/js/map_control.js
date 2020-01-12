618d76d4c35c0176164ded81525768430922f56a
Nikki <nikkimoteva@gmail.com>
2020-01-12 11:53:41 -0800

    pushing for backup

--- client/js/map_control.js
+++ client/js/map_control.js
@@ -1,5 +1,16 @@
-const map = new Datamap({ element: document.getElementById('choroplethMap') });
+const map = new Datamap({
+  element: document.getElementById('choroplethMap')
+});
+
 
 // object.onload = function(){
 
 // };
+
+// var basic_choropleth = new Datamap({
+//     element: document.getElementById("basic_choropleth"),
+//     projection: 'mercator',
+//     fills: {
+//       defaultFill: "#ABDDA4"
+//     }
+//   });
