37da33063c590933eeccc4c19c51df0e9cbfc287
noxolica <angelokn14@gmail.com>
2020-01-12 12:30:55 -0800

    event listener for form to trigger 'getData'

---
+++ client/js/main.js
@@ -0,0 +1,27 @@
+const countryInputForm = document.getElementById("countrySelect");
+
+function updateMap(e) {
+  console.log(e);
+
+  const countryInput = e.target.elements[0].value.toUpperCase();
+
+  const countryPromise = getSentiments(countryInput);
+
+  countryPromise
+    .then(countries => {
+      const updateMapObj = {}
+      countries.forEach(country => {
+        updateMapObj[country["target"]] = country["sentiment"]
+      })
+      return updateMapObj
+    })
+    .then(updateMapObj => { console.log(updateMapObj) })
+    .catch(err => { console.error(err) }
+    )
+
+  e.preventDefault();
+}
+
+countryInputForm.onsubmit = updateMap;
+
+
