27970edf45212bd0a01e84dd007aa988d81ce6fb
Nikki <nikkimoteva@gmail.com>
2020-01-12 10:05:06 -0800

    n/a

---
+++ config.py
@@ -0,0 +1,6 @@
+# Date: Saturday, January 12, 2020
+# Author: Nikki Moteva
+# Purpose: Save the KEY and ENDPOINT of the Microsoft Azure Cognitive Services used for project Mountain
+
+AZURE_API_KEY = "7cadd7aa228c4920b0b92cadcd7bb557"
+AZURE_API_ENDPOINT = "https://mountain.cognitiveservices.azure.com/"
