e1b5f21fdcddcd91e8caf9f333caa99998e433d1
noxolica <angelokn14@gmail.com>
2020-01-11 22:19:34 -0800

    added comments in main file

---
+++ py/main.py
@@ -0,0 +1,14 @@
+def main():
+  # news = get news generator
+  # process news ->
+  # for n in news:
+  #   POST(n, firebase_data)
+  #   POST(n, azure_ml)
+  #   .then(sentiment => {
+  #     POST(sentiment, firebase_sentiments)
+  #   })
+  #   .catch(err => print(err))
+
+
+if __name__ == "__main__":
+    main()
